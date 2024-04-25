4349081946545b2925bd316a0b4fb5078eeb7aec
Rodney Santiago <rodchar.owner@outlook.com>
2024-04-25 17:57:56 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEV
+            Console.WriteLine("Hello, DEV!");
+
+#elif PROD
+            Console.WriteLine("Hello, PROD!");
+#else
+            Console.WriteLine("Hello, Default!");
+
+#endif
+            Console.ReadLine();
+        }
+    }
+}
